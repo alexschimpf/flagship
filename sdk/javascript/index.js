ce5e158f1cc3cac638fd458c9998c096d7b0d005
Alex Schimpf <aschimpf1@gmail.coml>
2024-02-19 02:14:24 -0800

    more workflows...

--- sdk/javascript/index.js
+++ sdk/javascript/index.js
@@ -1,9 +1,5 @@
-import axios from 'axios/index';
-
-
 class Flagship {
-
-    /**
+  /**
      * @param host
      * @param projectId
      * @param userKey
@@ -13,24 +9,17 @@ class Flagship {
      *     This will be used by the Flagship API for authentication.
      * @param port
      */
-    constructor(host, projectId, userKey, signature, port = 443) {
-        this.projectId = projectId;
-        this.userKey = userKey;
-        this.signature = signature;
-        this.host = host;
-        this.scheme = port == 443 ? 'https' : 'http';
-        this.port = port;
+  constructor (host, projectId, userKey, signature, port = 443) {
+    this.projectId = projectId
+    this.userKey = userKey
+    this.signature = signature
+    this.host = host
+    this.scheme = port === 443 ? 'https' : 'http'
+    this.port = port
+    this.enabledFeatureFlags = new Set()
+  }
 
-        this.enabledFeatureFlags = new Set();
-
-        this.load = this.load.bind(this);
-        this._request = this._request.bind(this);
-        this.setEnabledFeatureFlags = this.setEnabledFeatureFlags.bind(this);
-        this.isFeatureFlagEnabled = this.isFeatureFlagEnabled.bind(this);
-        this.getEnabledFeatureFlags = this.getEnabledFeatureFlags.bind(this);
-    }
-
-    /**
+  /**
      * Loads the latest enabled feature flags for a user, given some context.
      * This will make an async request to the Flagship API.
      * This should be used sparingly - typically only in cases where the context changes.
@@ -41,56 +30,58 @@ class Flagship {
      *     Request timeout (in milliseconds)
      * @returns {Promise}
      */
-    async load(context, timeout = 60000) {
-        const response = await this._request(context, timeout);
-        const enabledFeatureFlags = response.data.feature_flags;
-        this.setEnabledFeatureFlags(enabledFeatureFlags);
-        return enabledFeatureFlags;
-    }
+  load = async (context, timeout = 60000) => {
+    const response = await this._request(context, timeout)
+    const enabledFeatureFlags = response.data.feature_flags
+    this.setEnabledFeatureFlags(enabledFeatureFlags)
+    return enabledFeatureFlags
+  }
 
-    _request(context, timeout) {
-        const url = `${this.scheme}://${this.host}:${this.port}/feature_flags`;
-        const params = {
-            'project_id': this.projectId,
-            'user_key': this.userKey
-        };
-        const headers = {
-            'Content-Type': 'application/json',
-            'Signature': this.signature
-        };
-        const data = JSON.stringify({
-            context: context
-        });
-        return axios.post(url, data, {
-            params,
-            headers,
-            timeout
-        });
-    }
-
-    /**
+  /**
      * Returns whether or not a feature flag is enabled for a user, given some context
      *
      * @param name: feature flag name
      * @returns {boolean}
      */
-    isFeatureFlagEnabled(name) {
-        return this.enabledFeatureFlags.has(name);
-    }
+  isFeatureFlagEnabled = (name) => {
+    return this.enabledFeatureFlags.has(name)
+  }
 
-    /**
+  /**
      * Returns all the enabled feature flags for a user, given some context
      */
-    getEnabledFeatureFlags() {
-        return this.enabledFeatureFlags;
-    }
+  getEnabledFeatureFlags = () => {
+    return this.enabledFeatureFlags
+  }
 
-    /**
+  /**
      * This can be used to bootstrap Flagship with some initial feature flags.
      */
-    setEnabledFeatureFlags(enabledFeatureFlags) {
-        this.enabledFeatureFlags = new Set(enabledFeatureFlags);
+  setEnabledFeatureFlags = (enabledFeatureFlags) => {
+    this.enabledFeatureFlags = new Set(enabledFeatureFlags)
+  }
+
+  _request = async (context, timeout) => {
+    const params = new URLSearchParams({
+      project_id: this.projectId,
+      user_key: this.userKey
+    })
+    const url = `${this.scheme}://${this.host}:${this.port}/feature_flags?${params}`
+    const headers = {
+      'Content-Type': 'application/json',
+      Signature: this.signature
     }
+    const data = JSON.stringify({
+      context
+    })
+    const response = await fetch(url, {
+      method: 'POST',
+      body: data,
+      headers,
+      signal: AbortSignal.timeout(timeout)
+    })
+    return response.json()
+  }
 }
 
-export default Flagship;
+export default Flagship
